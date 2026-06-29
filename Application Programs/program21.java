0d190fbbd18cfc5d3314ec86f24ad58b1d231cd6
Neha27-G <niagupta8@gmail.com>
2026-06-30 00:44:23 +0530

    sequence printing

---
+++ Application Programs/program21.java
@@ -0,0 +1,12 @@
+
+
+class program21
+{
+    public static void main(String A[])
+    {
+        System.out.println("Jay Ganesh..");
+        System.out.println("Jay Ganesh..");
+        System.out.println("Jay Ganesh..");
+        System.out.println("Jay Ganesh..");
+    }
+}
