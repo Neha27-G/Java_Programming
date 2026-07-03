71278dd80b9ebf5bee271821cb6b8af12c8bb881
Neha27-G <niagupta8@gmail.com>
2026-07-04 00:02:20 +0530

    program to print Divisible by 3 and 5

---
+++ Application Programs/program34.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class program34
+{
+    public static void main(String A[]) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0;
+
+        System.out.println("Enter the Number :");
+        iValue = sobj.nextInt();
+
+        if((iValue % 3 == 0) && (iValue % 5 ==0))
+        {
+            System.out.println("Number is Divisible by 3 and 5");
+        }
+        else
+        {
+            System.out.println("Number is not Divisible by 3 and 5");
+        }   
+    }
+}
