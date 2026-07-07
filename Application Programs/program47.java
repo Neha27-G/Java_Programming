e4230604fc32a56f2f0feab8d0f832375b6f6486
Neha27-G <niagupta8@gmail.com>
2026-07-07 22:24:59 +0530

    program to print jump to 2 in numbers

---
+++ Application Programs/program47.java
@@ -0,0 +1,35 @@
+// Input : 5
+// Output : 1  3   5
+
+import java.util.Scanner;
+
+class NumberX
+{
+    void Display(int iNo)
+    {
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt <= iNo; iCnt= iCnt + 2)
+        {
+            System.out.print(iCnt+"\t");
+        }
+        System.out.println("");
+    }
+}
+class program47
+{
+    public static void main(String  A[])
+    {
+        int iValue = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter the Number :");
+        iValue = sobj.nextInt();
+
+        NumberX nobj = new NumberX();
+
+        nobj.Display(iValue);
+
+    }
+}
