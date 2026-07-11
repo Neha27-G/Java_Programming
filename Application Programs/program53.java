a99fda1293a6fc6b7ec22b13bdb23beb47a8debb
Neha27-G <niagupta8@gmail.com>
2026-07-11 21:26:04 +0530

    program to  check Number is Prime or not

---
+++ Application Programs/program53.java
@@ -0,0 +1,54 @@
+
+import java.util.Scanner;
+
+class NumberX
+{
+    public boolean CheckPrime(int iNo)
+    {
+        int iCnt = 0;
+
+        for(iCnt = 2; iCnt <= iNo/2; iCnt++)
+        {
+            if((iNo % iCnt)==0)
+            {
+                break;
+            }
+        }
+
+        if(iCnt < iNo/2)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
+class program53
+{
+    public static void main(String  A[])
+    {
+        int iValue;
+        boolean bRet = false; 
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter the Number:");
+        iValue = sobj.nextInt();
+
+        NumberX nobj = new NumberX();
+
+        bRet = nobj.CheckPrime(iValue);
+
+        if(bRet == true)
+        {
+            System.out.println("It is a prime Number");
+        }
+        else
+        {
+            System.out.println("It is Not a prime Number");
+        }
+
+    }
+}
