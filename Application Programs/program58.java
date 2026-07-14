20d69411405f094725d74005944bf1e881862e60
Neha27-G <niagupta8@gmail.com>
2026-07-14 23:54:08 +0530

    Implementation of Do While Loop

---
+++ Application Programs/program58.java
@@ -0,0 +1,15 @@
+class program58
+{
+    public static void main(String  A[]) 
+    {
+        int iCnt = 0;
+
+        iCnt = 1;
+        do
+        {
+            System.out.println("Jay Ganesh...\n");
+            iCnt++; 
+        }while(iCnt <= 5);
+        
+    }
+}
