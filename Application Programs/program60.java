1cba01d4b23decd83a4c73f7116e77dcfc36810c
Neha27-G <niagupta8@gmail.com>
2026-07-14 23:55:48 +0530

    To print numbers n number of times in Reverse

---
+++ Application Programs/program60.java
@@ -0,0 +1,14 @@
+class program60
+{
+    public static void main(String  A[]) 
+    {
+        int iCnt = 0;
+
+        iCnt = 5;
+        while (iCnt >=1)
+        {
+            System.out.print(iCnt+"   ");
+            iCnt--; 
+        }
+    }
+}
