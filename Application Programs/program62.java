cc633eccdbb0e830d345a9c44a9ef50b0bab48e5
Neha27-G <niagupta8@gmail.com>
2026-07-14 23:53:41 +0530

    Implementation of While Loop

---
+++ Application Programs/program62.java
@@ -0,0 +1,15 @@
+class program62
+{
+    public static void main(String  A[]) 
+    {
+        int iDigit = 0;
+        int iNo = 751;
+
+        while(iNo != 0)
+        {
+            iDigit = iNo %  10;
+            System.out.println(iDigit+"\n");
+            iNo = iNo / 10;
+        }
+    }   
+}
