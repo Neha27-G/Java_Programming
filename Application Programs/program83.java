82aad7c3460dce105061aadc730ee75aa1f2f13d
Neha27-G <niagupta8@gmail.com>
2026-07-28 23:57:25 +0530

    Program based on  Array

---
+++ Application Programs/program83.java
@@ -0,0 +1,15 @@
+class program83
+{
+    static int Arr[] = new int[7];   
+
+    public static void main(String A[])
+    {
+        int Arr[] = {10,20,30,40,50};
+        int iCnt = 0;
+
+        for(iCnt = 0; iCnt < 5; iCnt++)
+        {
+            System.out.println(Arr[iCnt]);
+        }  
+    }
+}
