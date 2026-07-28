5e109722b16ebb5906a07bb7f1f902405ee14a95
Neha27-G <niagupta8@gmail.com>
2026-07-28 23:56:51 +0530

    Program based on  Array

---
+++ Application Programs/program80.java
@@ -0,0 +1,9 @@
+class program80
+{
+    public static void main(String A[])
+    {
+        int Arr[] = {10,20,30,40,50};
+
+        System.out.println(Arr.length);   // size of array             
+    }
+}
