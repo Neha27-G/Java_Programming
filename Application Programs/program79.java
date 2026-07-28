bd8ce0aa1b12097b887504ee8ec624bc46e6ef28
Neha27-G <niagupta8@gmail.com>
2026-07-28 23:56:41 +0530

    Program based on  Array

---
+++ Application Programs/program79.java
@@ -0,0 +1,10 @@
+class program79
+{
+    public static void main(String A[])
+    {
+        int Arr[] = {10,20,30,40,50};
+
+        System.out.println(Arr);                       //Reference represtation
+        System.out.println(Arr.hashCode());            // Hash code    ('&'dont work in java)
+    }
+}
