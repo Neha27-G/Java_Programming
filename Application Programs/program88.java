3b08463a0f9033dc1634b50b82d89a8292f38aca
Neha27-G <niagupta8@gmail.com>
2026-08-06 00:30:43 +0530

    program to print  base Address of Array

---
+++ Application Programs/program88.java
@@ -0,0 +1,17 @@
+
+class Program88
+{
+    static void Display(int Arr[])
+    {
+        System.out.println("First value of Arr : "+ Arr[0]);
+    }
+
+    public static void main(String A[])
+    {
+        int Brr[] = {10, 20, 30, 40, 50};
+
+        System.out.println("First Value of Brr : "+ Brr[0]);
+
+        Display(Brr);
+    }
+}
