4260a704954312d0497a66c6c54ef3542e18a26f
Neha27-G <niagupta8@gmail.com>
2026-08-06 00:31:30 +0530

    program based on Array

---
+++ Application Programs/program90.java
@@ -0,0 +1,22 @@
+
+class Program90
+{
+    static void Display(int Arr[])
+    {
+        int i = 0;
+
+        System.out.println(Arr[i]);
+        i++;
+        System.out.println(Arr[i]);
+        i++;
+        System.out.println(Arr[i]);
+        
+    }
+
+    public static void main(String A[])
+    {
+        int Brr[] = {10, 20, 30, 40, 50};
+
+        Display(Brr);
+    }
+}
