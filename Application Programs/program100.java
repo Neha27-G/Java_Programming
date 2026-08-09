51424869f68d47d709f89bfe4a9979950b8c4757
Neha27-G <niagupta8@gmail.com>
2026-08-09 22:33:38 +0530

    program based on Array

---
+++ Application Programs/program100.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class NumberX
+{
+    public void Display(int Arr[], int iSize)
+    {
+        int iCnt = 0;
+
+        System.out.println("Elements of Array are :");
+
+        for(iCnt = 0; iCnt < iSize; iCnt++)
+        {
+            System.out.println(Arr[iCnt]);
+        }
+
+    }
+}
+
+class program100
+{
+    public static void main(String A[])
+    {
+        int Brr[] = null;
+        int iLength = 0, i = 0;
+
+        Scanner sobj = new Scanner(System.in);  
+
+        System.out.println("Enter number of Elements are :");
+        iLength = sobj.nextInt();
+
+        Brr = new int[iLength];
+
+        System.out.println("Enter the elements :");
+
+        for(i = 0; i < iLength; i++) 
+        {
+            Brr[i] = sobj.nextInt();
+        }
+
+        NumberX nobj = new NumberX();
+
+        nobj.Display(Brr, iLength);
+
+        Brr = null;
+
+    }
+}
