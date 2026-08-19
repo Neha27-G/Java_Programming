1c08cda785bd6f6c36efe374f21d7d3873121454
Neha27-G <niagupta8@gmail.com>
2026-08-19 23:45:40 +0530

    Application-Based Program

--- Assignments/Assignment_52/program01.java
+++ Assignments/Assignment_52/program01.java
@@ -1,34 +1,55 @@
 import java.util.*;
 
+class FineCalculator
+{
+    public int CalculateFine(int daysKept)
+    {
+        if(daysKept < 0)
+        {
+            return -1;
+        }
+        else if(daysKept <= 7)
+        {
+            return 0;
+        }
+        else if(daysKept >= 8 && daysKept <= 12)
+        {
+            return (daysKept - 7) * 5;
+        }
+        else
+        {
+            return (5 * 5) + (daysKept - 12) * 10;
+        }   
+
+    }
+}
+
 class program01
 {
     public static void main(String A[]) 
     {
         int daysKept = 0;
-        int ifine= 0;
+        int iRet = 0;
 
         Scanner sobj = new Scanner(System.in);
 
         System.out.println("Enter the total number of days the book was kept :");
         daysKept = sobj.nextInt();
 
-        if(daysKept < 0)
+        FineCalculator fobj = new FineCalculator();
+        iRet = fobj.CalculateFine(daysKept);
+
+        if(iRet == -1)
         {
             System.out.println("Invalid input");
         }
-        else if(daysKept <= 7)
+        else if(iRet == 0)
         {
             System.out.println("Returned on Time. No fine applicable");
         }
-        else if(daysKept >= 8 && daysKept <= 12)
+        else 
         {
-            ifine = (daysKept - 7) * 5;
-            System.out.println("Total fine to be paid : Rs. "+ifine);
-        }
-        else
-        {
-            ifine = (5 * 5) + (daysKept - 12) * 10;
-            System.out.println("Total fine to be paid : Rs. "+ifine);
-        }   
+            System.out.println("Total fine to be paid : Rs. "+iRet);
+        } 
     }
 }
